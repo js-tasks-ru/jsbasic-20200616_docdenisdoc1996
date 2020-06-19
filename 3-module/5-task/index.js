17dfcee4fab4688e1fcb672aa252cecb005abee1
Denis Erkenov <derkenov@inbox.ru>
2020-06-19 21:23:01 +0300

    форматнул

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -4,14 +4,17 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
- let numbers = str.split(/[\s,]+/).filter(item => isFinite(item)).map(item => Number(item)).sort(compareNumeric);
+  let numbers = str.split(/[\s,]+/)
+    .filter(item => isFinite(item))
+    .map(item => Number(item))
+    .sort(compareNumeric);
 
- let result = {
-  min: numbers[0],
-  max: numbers[numbers.length -1]
-}
+  let result = {
+    min: numbers[0],
+    max: numbers[numbers.length - 1]
+  }
 
-return result;
+  return result;
 }
 
 function compareNumeric(a, b) {
