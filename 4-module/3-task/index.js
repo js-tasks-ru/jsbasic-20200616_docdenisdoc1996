b25212ac7fd8c21b619bf2abf8fa032bec43d2cf
Denis Erkenov <derkenov@inbox.ru>
2020-06-28 12:14:43 +0300

    4.3

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,39 @@
  * @param {Element} table
  */
 function highlight(table) {
-}
+    for (let r = 0; r < table.rows.length; r++) {
+        let tr = table.rows[r]
+        let td = table.rows[r].cells[3]
+        let gender = table.rows[r].cells[2].innerText
+        let age = table.rows[r].cells[1].innerText
+    
+        if (td.hasAttribute('data-available')) {
+
+            if (td.getAttribute('data-available') === 'true') {
+                tr.className +='available'
+            }
+              
+            if (td.getAttribute('data-available') === 'false') {
+                tr.className+='unavailable'
+            }
+          } else{
+              tr.setAttribute('hidden', true)
+          }
+ 
+          if (gender === 'm') {
+              tr.className += ' male'
+          } 
+
+          if (gender === 'f') {
+            tr.className += ' female'
+          }
+        
+          if (age < 18) {
+            tr.style = 'text-decoration: line-through'
+        }
+
+        console.log(tr)
+    }
+    }
+
+
